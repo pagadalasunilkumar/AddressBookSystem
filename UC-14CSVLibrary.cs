b5d61b10e0169a308f4d60593ec55d2b090af33a
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-06-24 11:58:55 +0530

    [sunil] the file is added

---
+++ UC-14CSVLibrary.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class UC_14CSVLibrary
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program!");
+
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("Enter Contact Details:");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            // Add contact to address book
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.AddContact(contact);
+
+            // Save address book to CSV
+            string csvFilePath = "addressbook.csv";
+            addressBook.SaveToCsv(csvFilePath);
+            Console.WriteLine("\nAddress book has been saved to the CSV file.");
+
+            // Read address book from CSV
+            AddressBook loadedAddressBook = AddressBook.LoadFromCsv(csvFilePath);
+            Console.WriteLine("\nAddress book has been loaded from the CSV file:");
+            loadedAddressBook.DisplayContacts();
+
+            Console.ReadLine();
+        }
+    }
+
+    class AddressBook
+    {
+        private List<Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void SaveToCsv(string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.WriteRecords(contacts);
+            }
+        }
+
+        public static AddressBook LoadFromCsv(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                var contacts = csv.GetRecords<Contact>();
+                AddressBook addressBook = new AddressBook();
+                addressBook.contacts.AddRange(contacts);
+                return addressBook;
+            }
+        }
+
+        public void DisplayContacts()
+        {
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact() { }
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Zip = zip;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Address: {Address}, City: {City}, State: {State}, Zip: {Zip}, Phone: {PhoneNumber}, Email: {Email}";
+        }
+    }
+}
