5a7701445388cd4926502a101767ef94349b1baa
pagadalasunilkumar <131426667+pagadalasunilkumar@users.noreply.github.com>
2023-06-26 07:54:32 +0530

    Update UC-9CollectionLibrary.cs

--- UC-9CollectionLibrary.cs
+++ UC-9CollectionLibrary.cs
@@ -9,7 +9,7 @@ namespace AddressBookSystem
     internal class UC_9CollectionLibrary
     {
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string Address { get; set;}
         public string City { get; set; }
         public string State { get; set; }
 
