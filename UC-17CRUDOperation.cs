c9110cee9186a31547d10ed1ad17db81f7bb1307
pagadalasunilkumar <131426667+pagadalasunilkumar@users.noreply.github.com>
2023-06-27 07:46:29 +0530

    Update UC-17CRUDOperation.cs

--- UC-17CRUDOperation.cs
+++ UC-17CRUDOperation.cs
@@ -13,7 +13,7 @@ namespace AddressBookSystem
     {
         public class Contact
         {
-            public string FirstName { get; set; }
+            public string FirstName { get; set;}
             public string LastName { get; set; }
             public string Address { get; set; }
             public string City { get; set; }
